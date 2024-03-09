9d1c2c9415be5d20a19bfec20d1be049719fde86
Jonas Haeberli <jonas.haeberli@hispeed.ch>
2024-03-09 11:41:47 +0100

    Fixed invers kinematics

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from two_dof_pick_and_place import two_dof_pick_and_place
 import numpy as np
 
 
-robot = two_dof_pick_and_place(250, 300, 100, np.deg2rad(10), 0, 0)
-test = robot.kinematics(kinematics_type="inverse", x=300, y=20)
+robot = two_dof_pick_and_place()
+test = robot.kinematics(kinematics_type="inverse", x=250, y=60)
 if test != "out_of_reach":
     print(np.rad2deg(test.get("upper_arm_angle")), np.rad2deg(test.get("lower_arm_angle")))
+    print(robot.draw_mechanism_with_inverse_kinematic(250, 60))
+    print(robot.kinematics(kinematics_type="forward", upper_arm_angle=test.get("upper_arm_angle"), lower_arm_angle=test.get("lower_arm_angle")))
 else:
     print("Error cought: out_of_reach")
--- two_dof_pick_and_place.py
+++ two_dof_pick_and_place.py
@@ -2,34 +2,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class two_dof_pick_and_place:
-    def __init__(self, upper_arm_length, lower_arm_length, lever_arm_length, lever_arm_angle_offset, gripper_x_offset, gripper_y_offset):
+    def __init__(self):
         """
-        Initializes the TwoDOFPickAndPlace class.
-
-        Args:
-            upper_arm_length (float): Length of the upper arm in millimeters.
-            lower_arm_length (float): Length of the lower arm in millimeters.
-            lever_arm_length (float): Length of the lever arm in millimeters.
-            lever_arm_angle_offset (float): Angle offset of the lever arm in radians.
-            gripper_x_offset (float): X offset of the gripper in millimeters.
-            gripper_y_offset (float): Y offset of the gripper in millimeters.
+        Initializes on Object of the two_dof_pick_and_place class.
         """
-        self.lower_arm_length = lower_arm_length
-        self.upper_arm_length = upper_arm_length
-        self.lever_arm_length = lever_arm_length
-        self.lever_arm_angle_offset = lever_arm_angle_offset
-        self.gripper_x_offset = gripper_x_offset
-        self.gripper_y_offset = gripper_y_offset
-        self.fig, self.ax = plt.subplots()
-
-
-    def start_plot(self):
-        self.ax.set_aspect('equal')
-        self.ax.set_xlim(-0.6, 0.6)
-        self.ax.set_ylim(-0.6,0.6)
-        self.ax.set_xlabel('X')
-        self.ax.set_ylabel('Y')
-        plt.grid(True)
+        self.upper_arm_length = 250
+        self.lower_arm_length = 300
+        self.lever_arm_length = 100
+        self.lever_arm_angle_offset = 0
+        self.gripper_x_offset = 0
+        self.gripper_y_offset = 0
+        self.x_rotation_offset = 0
+        self.y_rotation_offset = 0
+        self.upper_arm_range = (np.deg2rad(110), np.deg2rad(-5))
+        self.lower_arm_range = (np.deg2rad(10), np.deg2rad(-100))
+        self.min_lever_distance = 52.5
 
 
     def kinematics(self, kinematics_type="forward", x=None, y=None, upper_arm_angle=None, lower_arm_angle=None):
@@ -51,19 +38,16 @@ class two_dof_pick_and_place:
         """
         if kinematics_type == "forward":
             if upper_arm_angle is not None and lower_arm_angle is not None:
-                return { "x": self.upper_arm_length * np.cos(upper_arm_angle) + self.lower_arm_length * np.cos(lower_arm_angle) + self.gripper_x_offset,
-                        "y": self.upper_arm_length * np.sin(upper_arm_angle) + self.lower_arm_length * np.sin(lower_arm_angle) + self.gripper_y_offset}
+                return { "x": (self.upper_arm_length * np.cos(upper_arm_angle) + self.lower_arm_length * np.cos(lower_arm_angle + self.lever_arm_angle_offset) + self.x_rotation_offset + self.gripper_x_offset),
+                        "y": (self.upper_arm_length * np.sin(upper_arm_angle) + self.lower_arm_length * np.sin(lower_arm_angle + self.lever_arm_angle_offset) + self.y_rotation_offset + self.gripper_y_offset)}
             else:
                 print("Invalid forward kinematics arguments")
                 return    
         elif kinematics_type == "inverse":
             if x is not None and y is not None:
-                x = x - self.gripper_x_offset
-                y = y - self.gripper_y_offset
+                x = x - self.gripper_x_offset - self.x_rotation_offset
+                y = y - self.gripper_y_offset - self.y_rotation_offset
                 c = np.sqrt(x**2 + y**2)
-                if c > self.upper_arm_length + self.lower_arm_length or c < self.upper_arm_length - self.lower_arm_length:
-                    print("Position out of robot's reach!")
-                    return "out_of_reach"
                 upper_arm_angle = np.arccos((self.upper_arm_length**2 + c**2 - self.lower_arm_length**2) / (2 * self.upper_arm_length * c)) + np.arctan(y/x)
                 lower_arm_angle = upper_arm_angle + np.arccos((self.lower_arm_length**2 + self.upper_arm_length**2 - c**2) / (2 * self.lower_arm_length * self.upper_arm_length)) - np.pi
                 return { "upper_arm_angle": upper_arm_angle, "lower_arm_angle": lower_arm_angle} #in radians
@@ -74,16 +58,40 @@ class two_dof_pick_and_place:
             print("Invalid kinematics type")
             return
         
+    def draw_mechanism_with_inverse_kinematic(self, x, y):
+        # Calculate the inverse kinematic angles
+        x = x - self.gripper_x_offset
+        y = y - self.gripper_y_offset
+        c = np.sqrt(x**2 + y**2)
+        upper_arm_angle = np.arccos((self.upper_arm_length**2 + c**2 - self.lower_arm_length**2) / (2 * self.upper_arm_length * c)) + np.arctan(y/x)
+        lower_arm_angle = upper_arm_angle + np.arccos((self.lower_arm_length**2 + self.upper_arm_length**2 - c**2) / (2 * self.lower_arm_length * self.upper_arm_length)) - np.pi
+
+        return np.rad2deg(upper_arm_angle), np.rad2deg(lower_arm_angle)
     
     def check_position(self, x, y):
-        print("Checking position")
-    
-    
-    def remove_ploted_mechanism(self, ploted_lines):
-        for line in ploted_lines:
-            line.remove()
-        self.ax.clear()
-        self.fig.canvas.draw()
+        """
+        Check if a given position is within the robot's reach.
+
+        Parameters:
+        - x (float): The x-coordinate of the end effector position.
+        - y (float): The y-coordinate of the end effector position.
+
+        Returns:
+        - True if the position is within the robot's reach, False otherwise.
+        """
+        print(f"Checking position: {x}|{y}")
+        angles = self.kinematics(kinematics_type="inverse", x=x, y=y) != "out_of_reach"
+        if angles != "out_of_reach" and angles is not None:
+            if x >= 51.25: # Check Interfearance with Base mount
+                if self.upper_arm_range[0] >= angles.get("upper_arm_angle") >= self.upper_arm_range[1] and self.lower_arm_range[0] >= angles.get("lower_arm_angle") >= self.lower_arm_range[1]: # Check if angles are within allowable range
+                    print("Position is within allowable angle range")
+                    
+
+                    return True
+
+
+    def lever_distance (self, upper_arm_angle, lower_arm_angle):
+        return (np.sin(np.pi - upper_arm_angle - lower_arm_angle) * self.lever_arm_length)
 
 
     def draw_required_work_envelope(self, x_por_offset, y_por_offset):
@@ -93,8 +101,3 @@ class two_dof_pick_and_place:
 
         # Plot the work envelope
         self.ax.fill_between(x, y, color='skyblue', alpha=0.5)
-
-
-    def show_plot(self):
-        plt.show(block=False)
-        plt.pause(0.001)
