febf4e93dd045f264349391f5e0abc16be2bd1c7
Jonas Haeberli <jonas.haeberli@hispeed.ch>
2024-03-09 15:10:27 +0100

    Made Function to check Work envelope and export it

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from two_dof_pick_and_place import two_dof_pick_and_place
 import numpy as np
+import time
 
 
 robot = two_dof_pick_and_place()
-test = robot.kinematics(kinematics_type="inverse", x=225, y=60)
-if test != "out_of_reach":
-    print(np.rad2deg(test.get("upper_arm_angle")), np.rad2deg(test.get("lower_arm_angle")))
-    print(robot.kinematics(kinematics_type="forward", upper_arm_angle=test.get("upper_arm_angle"), lower_arm_angle=test.get("lower_arm_angle")))
-else:
-    print("Error cought: out_of_reach")
+robot.check_work_envelope()
--- two_dof_pick_and_place.py
+++ two_dof_pick_and_place.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import warnings
+import ezdxf
+from tqdm import tqdm
+from scipy.spatial import ConvexHull
+
 
 class two_dof_pick_and_place:
     def __init__(self):
-        """
-        Initializes on Object of the two_dof_pick_and_place class.
-        """
         self.upper_arm_length = 250
         self.lower_arm_length = 300
-        self.lever_arm_length = 100
+        self.lever_arm_length = 115
         self.lever_arm_angle_offset = np.deg2rad(-10)
         self.gripper_x_offset = 35
         self.gripper_y_offset = -16.25
@@ -18,24 +20,17 @@ class two_dof_pick_and_place:
         self.lower_arm_range = (np.deg2rad(10), np.deg2rad(-100))
         self.min_lever_distance = 52.5
 
+        # Work envelope parameters + calculation resolution
+        self.min_x = 50
+        self.max_x = 600
+        self.min_y = -400
+        self.max_y = 400
+        self.resolution = 5
+
+        warnings.filterwarnings("error", category=RuntimeWarning)
+
 
     def kinematics(self, kinematics_type="forward", x=None, y=None, upper_arm_angle=None, lower_arm_angle=None):
-        """
-        Perform forward or inverse kinematics calculations for a 2-DOF robot arm.
-
-        Parameters:
-        - kinematics_type (str): The type of kinematics calculation to perform. Valid values are "forward" and "inverse".
-        - x (float): The x-coordinate of the end effector position. Required for inverse kinematics.
-        - y (float): The y-coordinate of the end effector position. Required for inverse kinematics.
-        - upper_arm_angle (float): The angle of the upper arm in radians. Required for forward kinematics.
-        - lower_arm_angle (float): The angle of the lower arm in radians. Required for forward kinematics.
-
-        Returns:
-        - If kinematics_type is "forward" and valid angles are provided, returns a dictionary with the calculated x and y coordinates of the end effector.
-        - If kinematics_type is "inverse" and valid coordinates are provided, returns a dictionary with the calculated upper_arm_angle and lower_arm_angle in radians.
-        - If the arguments are invalid or the kinematics_type is invalid, prints an error message and returns None.
-        - If the position is out of the robot's reach, prints an error message and returns "out_of_reach".
-        """
         if kinematics_type == "forward":
             if upper_arm_angle is not None and lower_arm_angle is not None:
                 return { "x": (self.upper_arm_length * np.cos(upper_arm_angle) + self.lower_arm_length * np.cos(lower_arm_angle + self.lever_arm_angle_offset) + self.x_rotation_offset + self.gripper_x_offset),
@@ -58,46 +53,67 @@ class two_dof_pick_and_place:
             print("Invalid kinematics type")
             return
         
-    def draw_mechanism_with_inverse_kinematic(self, x, y):
-        # Calculate the inverse kinematic angles
-        x = x - self.gripper_x_offset
-        y = y - self.gripper_y_offset
-        c = np.sqrt(x**2 + y**2)
-        upper_arm_angle = np.arccos((self.upper_arm_length**2 + c**2 - self.lower_arm_length**2) / (2 * self.upper_arm_length * c)) + np.arctan(y/x)
-        lower_arm_angle = upper_arm_angle + np.arccos((self.lower_arm_length**2 + self.upper_arm_length**2 - c**2) / (2 * self.lower_arm_length * self.upper_arm_length)) - np.pi
-
-        return np.rad2deg(upper_arm_angle), np.rad2deg(lower_arm_angle)
     
     def check_position(self, x, y):
-        """
-        Check if a given position is within the robot's reach.
-
-        Parameters:
-        - x (float): The x-coordinate of the end effector position.
-        - y (float): The y-coordinate of the end effector position.
-
-        Returns:
-        - True if the position is within the robot's reach, False otherwise.
-        """
-        print(f"Checking position: {x}|{y}")
-        angles = self.kinematics(kinematics_type="inverse", x=x, y=y) != "out_of_reach"
-        if angles != "out_of_reach" and angles is not None:
+        try:
+            angles = self.kinematics(kinematics_type="inverse", x=x, y=y)
             if x >= 51.25: # Check Interfearance with Base mount
                 if self.upper_arm_range[0] >= angles.get("upper_arm_angle") >= self.upper_arm_range[1] and self.lower_arm_range[0] >= angles.get("lower_arm_angle") >= self.lower_arm_range[1]: # Check if angles are within allowable range
-                    print("Position is within allowable angle range")
-                    
-
                     return True
+        except RuntimeWarning as rw:
+            pass
+        return False
 
 
     def lever_distance (self, upper_arm_angle, lower_arm_angle):
-        return (np.sin(np.pi - upper_arm_angle - lower_arm_angle) * self.lever_arm_length)
+        print(np.rad2deg(np.pi-(-lower_arm_angle)-upper_arm_angle))
+        return np.sin(np.pi-(-lower_arm_angle)-upper_arm_angle) * self.lever_arm_length
 
 
-    def draw_required_work_envelope(self, x_por_offset, y_por_offset):
-        # Define the coordinates of the work envelope points
-        x = [0.16+x_por_offset, 0.16+x_por_offset, 0.5+x_por_offset, 0.32+x_por_offset, 0.16+x_por_offset,]
-        y = [0.06-y_por_offset, -0.0375-y_por_offset, -0.0375-y_por_offset, 0.06-y_por_offset, 0.06-y_por_offset,]
+    def check_work_envelope(self):
+        # Check points in possible work envelope
+        points = []
+        total_iterations = ((self.max_x - self.min_x) // (self.resolution/100)) * ((self.max_y - self.min_y) // (self.resolution/100))
+        progress_bar = tqdm(total=total_iterations, desc='Progress', unit=' iteration')
+
+        for x in range(self.min_x*100, self.max_x*100, self.resolution):
+            x = x / 100
+            for y in range(self.min_y*100, self.max_y*100, self.resolution):
+                y = y / 100
+                if self.check_position(x, y):
+                    points.append((x, y))
+                progress_bar.update(1)
+        progress_bar.close()
+
+        # Get max and min points for each x
+        y_values = {}
+        for x, y in points:
+            if x in y_values:
+                # Update the highest y value
+                y_values[x]['max'] = max(y_values[x]['max'], y)
+                # Update the lowest y value
+                y_values[x]['min'] = min(y_values[x]['min'], y)
+            else:
+                # Initialize the dictionary for this x
+                y_values[x] = {'max': y, 'min': y}
+        max_points = [(x, y_values[x]['max']) for x in y_values]
+        min_points = [(x, y_values[x]['min']) for x in y_values]
+
+        # Create the dxf file
+        doc = ezdxf.new()
+        msp = doc.modelspace()
+        for i in range(len(max_points) - 1):
+            msp.add_line(max_points[i], max_points[i + 1])
+        for i in range(len(min_points) - 1):
+            msp.add_line(min_points[i], min_points[i + 1])
+        msp.add_line(max_points[0], min_points[0])
+        msp.add_line(max_points[-1], min_points[-1])
+        doc.saveas("plot.dxf")
+        
+
+    def draw_required_work_envelope(self):
+        x = [160, 160, 500, 320, 160]
+        y = [60, -37.5, -37.5, 60, 60]
 
         # Plot the work envelope
-        self.ax.fill_between(x, y, color='skyblue', alpha=0.5)
+        self.axs[0].fill_between(x, y, color='skyblue', alpha=0.5)
