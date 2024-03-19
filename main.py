1ea96f18aa294ac650cc0381178377d68b528e25
Jonas Haeberli <jonas.haeberli@hispeed.ch>
2024-03-19 22:39:33 +0100

    test

--- main.py
+++ main.py
@@ -6,10 +6,31 @@ import time
 pick_and_place = robot()
 pick_and_place.hardware.setup()
 
-pick_and_place.hardware.move_dummy(110, 0)
-input("Press Enter to move...")
-pick_and_place.hardware.move_to(110, 0)
+input("Press Enter to go to start position...")
 
-pick_and_place.hardware.move_dummy(90, 0)
-input("Press Enter to move...")
-pick_and_place.hardware.move_to(90, 0)
+if pick_and_place.kinematics.check_position(200, 50):
+    start_pos = pick_and_place.kinematics.kinematics(kinematics_type="inverse", x=200, y=50)
+    pick_and_place.hardware.move_to(np.rad2deg(start_pos.get("upper_arm_angle")), np.rad2deg(start_pos.get("lower_arm_angle")))
+
+input("Press Enter to start the pick and place routine...")
+
+for i in range (200,300):
+    if pick_and_place.kinematics.check_position(i, 50):
+        pos = pick_and_place.kinematics.kinematics(kinematics_type="inverse", x=i, y=50)
+        pick_and_place.hardware.move_to(np.rad2deg(pos.get("upper_arm_angle")), np.rad2deg(pos.get("lower_arm_angle")))
+
+for i in range (50, 150):
+    if pick_and_place.kinematics.check_position(300, i):
+        pos = pick_and_place.kinematics.kinematics(kinematics_type="inverse", x=300, y=i)
+        pick_and_place.hardware.move_to(np.rad2deg(pos.get("upper_arm_angle")), np.rad2deg(pos.get("lower_arm_angle")))
+
+for i in range (300, 200, -1):
+    if pick_and_place.kinematics.check_position(i, 150):
+        pos = pick_and_place.kinematics.kinematics(kinematics_type="inverse", x=i, y=150)
+        pick_and_place.hardware.move_to(np.rad2deg(pos.get("upper_arm_angle")), np.rad2deg(pos.get("lower_arm_angle")))
+
+for i in range (150, 50, -1):
+    if pick_and_place.kinematics.check_position(200, i):
+        pos = pick_and_place.kinematics.kinematics(kinematics_type="inverse", x=200, y=i)
+        pick_and_place.hardware.move_to(np.rad2deg(pos.get("upper_arm_angle")), np.rad2deg(pos.get("lower_arm_angle")))
+
