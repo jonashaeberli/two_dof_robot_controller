9c500dd640c68ccaf7b4b6db788439679b1377fe
HDJG <118261566+HDJGJonas@users.noreply.github.com>
2024-03-19 22:27:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import time
 pick_and_place = robot()
 pick_and_place.hardware.setup()
 
-pick_and_place.hardware.move_dummy(110, 0)
+pick_and_place.hardware.move_dummy(110, 5)
 input("Press Enter to move...")
-pick_and_place.hardware.move_to(110, 0)
+pick_and_place.hardware.move_to(110, 5)
 
-pick_and_place.hardware.move_dummy(90, 0)
+pick_and_place.hardware.move_dummy(90, -5)
 input("Press Enter to move...")
-pick_and_place.hardware.move_to(90, 0)
+pick_and_place.hardware.move_to(90, -5)
