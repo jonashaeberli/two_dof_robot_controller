f8a12543effdb326ca29290aa507a8ad83b46bd4
Jonas Haeberli <jonas.haeberli@hispeed.ch>
2024-03-23 20:53:37 +0100

    changed code for arduino

--- gripper_arduino/gripper_arduino.ino
+++ gripper_arduino/gripper_arduino.ino
@@ -1,11 +1,19 @@
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  pinMode(7, OUTPUT);
+  digitalWrite(7, LOW); // Initialize pin 7 as HIGH
 }
 
 void loop() {
   if (Serial.available() > 0) {
     String data = Serial.readStringUntil('\n');
-    Serial.print("You sent me: ");
-    Serial.println(data);
+    data.trim(); // Remove any leading/trailing whitespace
+    if (data == "open") {
+      digitalWrite(7, LOW); // Set pin 7 to LOW
+      Serial.println("gripper opened");
+    } else if (data == "close") {
+      digitalWrite(7, HIGH); // Set pin 7 to HIGH
+      Serial.println("gripper closed");
+    }
   }
 }
