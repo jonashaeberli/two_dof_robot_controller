779e91ab5229baed2b58a0e262eb1aa2712647c9
HDJG <118261566+HDJGJonas@users.noreply.github.com>
2024-03-24 13:17:49 +0100

    Update controller.py

--- controller.py
+++ controller.py
@@ -252,9 +252,9 @@ class controller:
                     actual_pos = self.hardware.get_angles()
 
                     # Check if the actual position is within 2% of the commanded position
-                    if abs(actual_pos['upper_arm_angle'] - pos.get('upper_arm_angle')) > 0.02 * pos.get('upper_arm_angle') or \
-                       abs(actual_pos['lower_arm_angle'] - pos.get('lower_arm_angle')) > 0.02 * pos.get('lower_arm_angle'):
-                        print("Error: Actual position is more than 0.5% out of the commanded position")
+                    if abs(actual_pos['upper_arm_angle'] - pos.get('upper_arm_angle')) > 0.1 * pos.get('upper_arm_angle') or \
+                       abs(actual_pos['lower_arm_angle'] - pos.get('lower_arm_angle')) > 0.1 * pos.get('lower_arm_angle'):
+                        print("Error: Actual position is more than 10% out of the commanded position")
                         return
                     
                     elapsed_time = end_time - start_time  # calculate elapsed time
