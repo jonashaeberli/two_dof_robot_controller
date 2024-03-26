b1fbea5180f50be35b8cff003909316cfefd773b
HDJG <118261566+HDJGJonas@users.noreply.github.com>
2024-03-26 08:52:24 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,14 +4,14 @@ import time
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--vel', type=int, default=50, help='Velocity')
+parser.add_argument('--vel', type=int, default=10, help='Velocity')
 parser.add_argument('--accel', type=int, default=50, help='Acceleration')
-parser.add_argument('--calib', type=str, default='False', help='Run calibration')
+parser.add_argument('--calib', type=str, default='True', help='Run calibration')
 args = parser.parse_args()
 
 args.calib = args.calib.lower() in ['true', '1', 't', 'y', 'yes']
 
-pnp = robot(run_sim=True)
+pnp = robot()
 pnp.controller.set_move_params(args.vel, args.accel)
 if pnp.controller.setup_controller():
     if args.calib:
@@ -26,22 +26,12 @@ if pnp.controller.setup_controller():
     pnp.controller.moveL(500, 50)
     pnp.controller.pause_trajectory(0.2)
     pnp.controller.set_gripper("close")
-    if pnp.controller.validate_trajectory():
-        print("Trajectory is valid")
-        print(pnp.controller.calculate_max_velocity_and_acceleration())
-        input("Press Enter to execute pnp trajectory")
-        pnp.controller.execute_trajectory()
     pnp.controller.pause_trajectory(0.2)
     pnp.controller.moveL(500, 80)
     pnp.controller.moveL(150, 80)
     pnp.controller.moveL(150, 50)
     pnp.controller.pause_trajectory(0.2)
     pnp.controller.set_gripper("open")
-    if pnp.controller.validate_trajectory():
-        print("Trajectory is valid")
-        print(pnp.controller.calculate_max_velocity_and_acceleration())
-        input("Press Enter to execute pnp trajectory")
-        pnp.controller.execute_trajectory()
     pnp.controller.pause_trajectory(0.2)
     pnp.controller.moveL(150, 80)
 
@@ -50,22 +40,12 @@ if pnp.controller.setup_controller():
     pnp.controller.moveL(475, 50)
     pnp.controller.pause_trajectory(0.2)
     pnp.controller.set_gripper("close")
-    if pnp.controller.validate_trajectory():
-        print("Trajectory is valid")
-        print(pnp.controller.calculate_max_velocity_and_acceleration())
-        input("Press Enter to execute pnp trajectory")
-        pnp.controller.execute_trajectory()
     pnp.controller.pause_trajectory(0.2)
     pnp.controller.moveL(475, 80)
     pnp.controller.moveL(175, 80)
     pnp.controller.moveL(175, 50)
     pnp.controller.pause_trajectory(0.2)
     pnp.controller.set_gripper("open")
-    if pnp.controller.validate_trajectory():
-        print("Trajectory is valid")
-        print(pnp.controller.calculate_max_velocity_and_acceleration())
-        input("Press Enter to execute pnp trajectory")
-        pnp.controller.execute_trajectory()
     pnp.controller.pause_trajectory(0.2)
     pnp.controller.moveL(175, 120)
 
@@ -74,22 +54,12 @@ if pnp.controller.setup_controller():
     pnp.controller.moveL(150, 50)
     pnp.controller.pause_trajectory(0.2)
     pnp.controller.set_gripper("close")
-    if pnp.controller.validate_trajectory():
-        print("Trajectory is valid")
-        print(pnp.controller.calculate_max_velocity_and_acceleration())
-        input("Press Enter to execute pnp trajectory")
-        pnp.controller.execute_trajectory()
     pnp.controller.pause_trajectory(0.2)
     pnp.controller.moveL(150, 120)
     pnp.controller.moveL(500, 120)
     pnp.controller.moveL(500, 50)
     pnp.controller.pause_trajectory(0.2)
     pnp.controller.set_gripper("open")
-    if pnp.controller.validate_trajectory():
-        print("Trajectory is valid")
-        print(pnp.controller.calculate_max_velocity_and_acceleration())
-        input("Press Enter to execute pnp trajectory")
-        pnp.controller.execute_trajectory()
     pnp.controller.pause_trajectory(0.2)
     pnp.controller.moveL(500, 80)
 
@@ -98,33 +68,19 @@ if pnp.controller.setup_controller():
     pnp.controller.moveL(175, 50)
     pnp.controller.pause_trajectory(0.2)
     pnp.controller.set_gripper("close")
-    if pnp.controller.validate_trajectory():
-        print("Trajectory is valid")
-        print(pnp.controller.calculate_max_velocity_and_acceleration())
-        input("Press Enter to execute pnp trajectory")
-        pnp.controller.execute_trajectory()
     pnp.controller.pause_trajectory(0.2)
     pnp.controller.moveL(175, 80)
     pnp.controller.moveL(475, 80)
     pnp.controller.moveL(475, 50)
     pnp.controller.pause_trajectory(0.2)
     pnp.controller.set_gripper("open")
-    if pnp.controller.validate_trajectory():
-        print("Trajectory is valid")
-        print(pnp.controller.calculate_max_velocity_and_acceleration())
-        input("Press Enter to execute pnp trajectory")
-        pnp.controller.execute_trajectory()
     pnp.controller.pause_trajectory(0.2)
     pnp.controller.moveL(475, 80)
     pnp.controller.moveL(150, 80)
-    if pnp.controller.validate_trajectory():
-        print("Trajectory is valid")
-        print(pnp.controller.calculate_max_velocity_and_acceleration())
-        input("Press Enter to execute pnp trajectory")
-        pnp.controller.execute_trajectory()
 
     if pnp.controller.validate_trajectory():
         print("Trajectory is valid")
         print(pnp.controller.calculate_max_velocity_and_acceleration())
         input("Press Enter to execute pnp trajectory")
         pnp.controller.execute_trajectory()
+        input("Press Enter to close")
